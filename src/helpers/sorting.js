2a58d06f63afc01e3bf34fade01e3a832b2b56ac
Andrey Pechennikov <ap@devandy.me>
2022-01-21 17:01:59 +0600

    Исправил сортировку по дате

--- src/helpers/sorting.js
+++ src/helpers/sorting.js
@@ -1,6 +1,8 @@
+import dayjs from 'dayjs';
+
 const sortMoviesByRating = (movies) => movies.sort(({rating: firstRating}, {rating: secondRating}) => secondRating - firstRating);
 const sortMoviesByComments = (movies) => movies.sort(({comments: firstComments}, {comments: secondComments}) => secondComments.length - firstComments.length);
-const sortMoviesByDate = (movies) => movies.sort(({releaseDate: firstDate}, {releaseDate: secondDate}) => secondDate - firstDate);
+const sortMoviesByDate = (movies) => movies.sort(({releaseDate: firstDate}, {releaseDate: secondDate}) => dayjs(secondDate).diff(dayjs(firstDate)));
 const sortChartGenres = (chartData) => {
   const genres = Object.keys(chartData);
 
